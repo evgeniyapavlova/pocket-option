4bd38bbafcdaa5fe1efed80a4342485efc906580
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-05-27 17:13:53 +0300

    Add section 4 for main page

--- src/components/main-page/MainPage.svelte
+++ src/components/main-page/MainPage.svelte
@@ -2,8 +2,10 @@
 	import Intro from './Section1Intro.svelte';
 	import Section2Reliability from './Section2Reliability.svelte';
 	import Section3Reputation from './Section3Reputation.svelte';
+	import Section4Safety from './Section4Safety.svelte';
 </script>
 
 <Intro />
 <Section2Reliability />
 <Section3Reputation />
+<Section4Safety />
---
+++ src/components/main-page/Section4Safety.svelte
@@ -0,0 +1,89 @@
+<script>
+	const items = [
+		'Deposit Insurance',
+		'Guaranteed In-Time Execution for Your Trading Orders',
+		"Advanced Security Measures for Protecting Client's Data"
+	];
+</script>
+
+<div class="content">
+	<h2>Safety and Security</h2>
+	<div class="text">
+		We at PocketOption prioritize your safety and security. <br />Our well-developed authorization
+		system guarantees the utmost protection for your account. We ensure:
+	</div>
+
+	<div class="safety-items">
+		{#each items as item, index}
+			<div class="safety-item">
+				<div
+					class="safety-img"
+					style="background-image: url('/images/main-page/safety-item-{index + 1}.png')"
+				></div>
+				<div class="text">{item}</div>
+			</div>
+		{/each}
+	</div>
+
+	<div class="text">
+		Your deposits and personal information are safeguarded with <span class="ws-nowrap"
+			>state-of-the-art</span
+		> technology.
+	</div>
+</div>
+
+<style>
+	.safety-img {
+		position: absolute;
+		width: 132px;
+		height: 132px;
+		background-size: cover;
+		top: -70px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	.safety-items {
+		display: flex;
+		align-items: flex-start;
+		justify-content: space-around;
+		margin-top: 124px;
+		margin-bottom: 50px;
+	}
+	.safety-item {
+		background-color: #0c1526;
+		padding: 70px 30px 0;
+		width: 270px;
+		height: 177px;
+		border-radius: 15px;
+		border: 1px solid var(--highlight-blue);
+		position: relative;
+	}
+	.content {
+		text-align: center;
+		padding-bottom: 140px;
+	}
+	.text {
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	@media screen and (max-width: 1100px) {
+		.safety-items {
+			justify-content: space-between;
+		}
+	}
+
+	@media screen and (max-width: 960px) {
+		.safety-items {
+			display: block;
+			margin-bottom: -50px;
+		}
+		.safety-item {
+			margin: 0 auto 92px;
+			width: 250px;
+		}
+		.content {
+			padding-bottom: 100px;
+		}
+	}
+</style>
